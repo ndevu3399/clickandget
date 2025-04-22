e7b0128cd0dd427079d176a5abeec2637744eaef
ndevu3399 <wachiramwas122@gmail.com>
2025-04-22 22:33:28 +0300

    refactor main.jsx to use ReactDOM and wrap App in CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import 'bulma/css/bulma.min.css'
+import { CartProvider } from './context/CartContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>,
 )
