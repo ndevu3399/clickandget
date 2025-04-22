1356c64b5e30ce6ab44c9567c89173d36d878e6f
ndevu3399 <wachiramwas122@gmail.com>
2025-04-22 22:37:27 +0300

    add fetchProducts function to retrieve product data from API

---
+++ services/api.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://dummyjson.com/products'
+
+export const fetchProducts = async () => {
+  const res = await fetch(API_URL)
+  const data = await res.json()
+  return data.products
+}
