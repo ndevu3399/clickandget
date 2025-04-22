456ec58c52aa0249b55911cddbccf61db729966b
ndevu3399 <wachiramwas122@gmail.com>
2025-04-22 22:43:25 +0300

    add CartPage component to display the cart with Cart component

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cart from '../components/Cart'
+
+export default function CartPage() {
+  return (
+    <div>
+      <h1 className="title is-3 mb-5">Your Cart</h1>
+      <Cart />
+    </div>
+  )
+}
