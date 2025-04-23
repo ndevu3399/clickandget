12f6003e38190c052c7e0457c1e47898eab909cd
ndevu3399 <wachiramwas122@gmail.com>
2025-04-23 23:22:21 +0300

    style: add NotFound page for 404 error handling

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="has-text-centered">
+      <h1 className="title is-3">404 - Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+    </div>
+  );
+}
