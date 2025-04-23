4f538520d34333d4576d4787374691947e796a41
ndevu3399 <wachiramwas122@gmail.com>
2025-04-23 23:20:35 +0300

    style: add NotFound page for 404 fallback routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import CartPage from './pages/CartPage.jsx'
 import Header from './components/Header.jsx'
+import NotFound from './pages/NotFound.jsx'  // Importing a NotFound page for 404
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="*" element={<NotFound />} /> {/* 404 fallback */}
         </Routes>
       </div>
     </Router>
