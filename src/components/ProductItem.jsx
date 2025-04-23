9d9980e1d8c15b817e09e5a8056f1a010516d8af
ndevu3399 <wachiramwas122@gmail.com>
2025-04-23 23:07:51 +0300

    style: enhance ProductItem component with improved styling and layout

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,16 +5,51 @@ export default function ProductItem({ product }) {
   const { addToCart } = useContext(CartContext)
 
   return (
-    <div className="card">
+    <div
+      className="card product-card"
+      style={{
+        padding: '1rem',
+        backgroundColor: '#ffffff',
+        border: 'none',
+      }}
+    >
       <div className="card-image">
-        <figure className="image is-4by3">
-          <img src={product.thumbnail} alt={product.title} />
+        <figure className="image is-4by3" style={{ borderRadius: '10px' }}>
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            style={{ borderRadius: '10px' }}
+          />
         </figure>
       </div>
       <div className="card-content">
-        <p className="title is-6">{product.title}</p>
-        <p className="subtitle is-6">${product.price}</p>
-        <button className="button is-primary" onClick={() => addToCart(product)}>Add to Cart</button>
+        <p
+          className="title is-6 has-text-black is-uppercase"
+          style={{ fontWeight: '600', letterSpacing: '0.5px' }}
+        >
+          {product.title}
+        </p>
+        <p
+          className="subtitle is-6 has-text-grey-dark"
+          style={{ fontSize: '1rem', fontWeight: '700' }}
+        >
+          ${product.price}
+        </p>
+        <button
+          className="button is-primary is-fullwidth is-small"
+          style={{
+            backgroundColor: '#111111',
+            color: '#fff',
+            borderRadius: '50px',
+            padding: '12px',
+            fontSize: '1rem',
+            letterSpacing: '0.5px',
+            transition: 'background-color 0.3s',
+          }}
+          onClick={() => addToCart(product)}
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   )
